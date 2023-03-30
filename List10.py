bc282ed0ce1a79ac23b3c75ce117a8cfebf20e4e
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-30 08:11:17 +0500

    List10

--- List10.py
+++ List10.py
@@ -6,4 +6,19 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+
+    i = 0
+    b = 0
+    n = 0
+
+    while i < len(list1):
+        if list1[i] == 1:
+           b += 1
+        
+        elif list1[i] == 0:
+            n += 1
+            
+        i += 1
+    return f"{[b, n]}"
+
+print(main([1, 1, 0, 0, 1, 0, 1, 0]))
